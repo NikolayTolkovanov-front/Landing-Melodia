16eec3bc1e2ee5b8a70af4c1680e8ffe38a8a3b9
Николай <71602188+NikolayTolkovanov@users.noreply.github.com>
2021-08-21 13:38:41 +0300

    Добавлено модальное окно

--- js/main.js
+++ js/main.js
@@ -10881,49 +10881,105 @@ return jQuery;
 } );
 
 $(() => {
+// Переменные для main
   let currentFloor = 2
   let counterSpan = $(".count__pick-counter")
-  let flat = $(".flats path")
+  let floorPath = $(".flats path")
 
   let arrowUp = $(".count__pick-arrow--up")
   let arrowDown = $(".count__pick-arrow--down")
 
-  counterSpan.text(currentFloor)
+// // Пееременные для modal
+//   let currentFlat = 2
+//   let modalWindow = $('.modal')
+//   let btnFlats = $('.btn-flats')
+//   let flatsPath = $('.modal__floor-floor path')
+//   let modalBtnClose = $('.modal__exit')
+//   let modalCounter = $('.modal__floor-counter')
 
-  flat.on("mousemove", function () {
-    flat.removeClass('current-floor')
-    currentFloor = $(this).attr("data-floor")
+
+
+// // Настройка main
+  function changeBtnFlat() {
+    let usCurrentFloor = currentFloor.toLocaleString("en-US", {
+        minimumIntegerDigits: 2,
+        useGrouping: false,
+    })
+
+    counterSpan.text(usCurrentFloor)
+    floorPath.removeClass('current-floor')
+    $(`[data-floor=${usCurrentFloor}]`).toggleClass('current-floor')
+  }
+  
+// Начальное значение счетчика
+  counterSpan.text(currentFloor.toLocaleString("en-US", {
+    minimumIntegerDigits: 2,
+    useGrouping: false,
+}))
+
+// Изменение текста
+  floorPath.on("mousemove", function () {
+    floorPath.removeClass('current-floor')
+    currentFloor = $(this).attr('data-floor')
     counterSpan.text(currentFloor)
   })
 
+
+// Настройка стрелок
   arrowUp.on("click", function () {
     if (currentFloor < 18) {
       currentFloor++
-      
-      let usCurrentFloor = currentFloor.toLocaleString("en-US", {
-        minimumIntegerDigits: 2,
-        useGrouping: false,
-      })
-    
-      counterSpan.text(usCurrentFloor)
-      counterSpan.text(usCurrentFloor)
-      flat.removeClass('current-floor')
-      $(`[data-floor=${usCurrentFloor}]`).toggleClass('current-floor')
+      changeBtnFlat()
     }
   })
 
   arrowDown.on("click", function () {
     if (currentFloor > 2) {
       currentFloor--
-
-      let usCurrentFloor = currentFloor.toLocaleString("en-US", {
-        minimumIntegerDigits: 2,
-        useGrouping: false,
-      })
-
-      counterSpan.text(usCurrentFloor)
-      flat.removeClass('current-floor')
-      $(`[data-floor=${usCurrentFloor}]`).toggleClass('current-floor')
+      changeBtnFlat()
     }
   })
+
+
+// Пееременные для modal
+  let modalWindow = $('.modal')
+  let modalCounter = $('.modal__floor-counter')
+  let btnFlats = $('.btn-flats')
+  let modalBtnClose = $('.modal__exit')
+  let flatsPath = $('.modal__floor-floor path')
+  let linksPath = $('.modal__picker-list a')
+  let linksList = $('.modal__picker-list')
+  let currentFlat = 2
+  let currentLink = 2
+
+// // Настройка modal
+  btnFlats.on('click', function() {
+    modalWindow.css('display', 'block')
+    modalCounter.text(Number(currentFloor))
+  })
+
+  modalBtnClose.on('click', function () {
+    modalWindow.css('display', 'none')
+  })
+
+  flatsPath.on('mousemove', function() {
+    currentFlat = $(this).attr('data-flat')
+    linksPath.removeClass('modal__current-link')
+    $(linksPath[currentFlat - 1]).addClass('modal__current-link')
+  })
+
+  flatsPath.on('mouseleave', function() {
+    $(linksPath[currentFlat - 1]).removeClass('modal__current-link')
+  })
+  console.log(linksPath);
+
+  linksPath.on('mousemove', function() {
+    currentLink = linksPath.index(this) + 1
+    flatsPath.removeClass('current-flat')
+    $(`[data-flat="${currentLink}"]`).toggleClass('current-flat')
+  })
+})
+
+$(() => {
+
 })
